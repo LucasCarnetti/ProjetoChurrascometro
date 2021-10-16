4c5af604549c402ec7cc8de42a8d00b208ccd2f7
Lucas Carnetti <lucas.carnetti@gmail.com>
2021-10-16 19:00:05 -0300

    Falta colocar modo de preparo

--- script/script.js
+++ script/script.js
@@ -7,6 +7,7 @@
 const adultsInput = document.getElementById('adults');
 const childrenInput = document.getElementById('childrens');
 const hoursInput = document.getElementById('hours');
+const sugs = document.getElementById('check')
 
 const result = document.getElementById('result');
 
@@ -24,15 +25,22 @@ function printResult(){
         location.reload();
     }
     else{
+    
     let qtdCarneTotal = (carnePP(qtdHours) * qtdAdults + (carnePP(qtdHours) / 2 * qtdChildren)) /1000
     let qtdCervejaTotal = ((cervejaPP(qtdHours) * qtdAdults)/355)
     let qtdBebidasTotal = ((bebidasPP(qtdHours) * qtdAdults)+ (bebidasPP(qtdHours)/2 * qtdChildren)) /1000
+    let randomSugs = Math.floor(Math.random()*10)//cria numero randomico para selecionar um index do array que vai conter as receitas
+    let ingredientes = sugestion(qtdCarneTotal, randomSugs)
+    
     result.innerHTML = '<p>'+qtdCarneTotal+'Kg de Carne</p><p>'
     +Math.ceil(qtdCervejaTotal)+' Latas(355ml) de cerveja</p><p>'
     +qtdBebidasTotal+' Litros de bebidas não alcoolicas</p>'
+    if(sugs.checked){
+        result.innerHTML += '<p>'+ingredientes+'</p>'
+        }
+    
     }
 }
-
 function carnePP(qtdHours){
     if(qtdHours >= 6){
         return 650;
@@ -56,4 +64,21 @@ function bebidasPP(qtdHours){
     else{
         return 1000
     }
+}
+function sugestion(qtdCarneTotal, randomSugs){
+    
+    let primeiraSugs = "<br>Porco e Alcatra no espeto</p><p>Ingredientes:<p>"+(qtdCarneTotal * 0.6)+"Kg de Lombo de porco</p><p>"+(qtdCarneTotal * 0.2)+"Kg de linguicinha toscana</p><p>Vinagre de vinho tinto</p><p>Cebola, alho e sal a gosto</p>"
+    let segundaSugs = "<br>Churrasco de Carneiro</p><p>Ingredientes:<p>"+(qtdCarneTotal)+"Kg de Carne de carneiro cortado em pedaços médios</p><p>Vinho branco Seco</p><p>1/2 copo de vinagre branco</p><p>1 cabeça de alho</p><p>Cebolas a gosto</p><p>1 xícara de azeite</p><p>Alecrim, pimenta do reino e sal a gosto</p>"
+    let terceiraSugs = "<br>Picanha no forno</p><p>Ingredientes:<p>"+(qtdCarneTotal)+"Kg de Picanha</p><p>Sal grosso temperado</p>"
+    let quartaSugs = "<br>Picanha Especial</p><p>Ingredientes:<p>"+(qtdCarneTotal)+"Kg de Picanha</p><p>Sal grosso temperado</p>"
+    let quintaSugs = "<br>Churrasco gaúcho com aperitivo de linguiça toscana</p><p>Ingredientes:<p>"+(qtdCarneTotal * 0.8)+"Kg de Costela de gado</p><p>"+(qtdCarneTotal * 0.2)+"Kg de linguiça toscana</p><p>Sal grosso</p>"
+    let sextaSugs = "<br>Espetinhos de coxão mole em cubos</p><p>Ingredientes:<p>"+(qtdCarneTotal * 0.8)+"kg de Coxão mole em cubos</p><p>"+(qtdCarneTotal * 0.2)+"Kg de bacon em tiras</p><p>Espetinhos de madeira</p><p>Pimenta do reino, Azeite e Sal a gosto</p>"
+    let setimaSugs =  "<br>Picanha na manteiga verde</p><p>Ingredientes:<p>"+(qtdCarneTotal)+"kg de Picanha</p><p>"+(qtdCarneTotal * 0.25)+"Kg de Manteiga com sal</p><p>"+(qtdCarneTotal * 0.1)+"Kg de Alho sem casca</p><p>Salsa, Cebolinha e Sal a gosto"
+    let oitavaSugs = "<br>Churrasco gaúcho com aperitivo de linguiça toscana</p><p>Ingredientes:<p>"+(qtdCarneTotal * 0.8)+"Kg de Costela de gado</p><p>"+(qtdCarneTotal * 0.2)+"Kg de linguiça toscana</p><p>Sal grosso</p>"
+    let nonaSugs = "<br>Picanha Especial</p><p>Ingredientes:<p>"+(qtdCarneTotal)+"Kg de Picanha</p><p>Sal grosso temperado</p>"
+    let decimaSugs = "<br>Porco e Alcatra no espeto</p><p>Ingredientes:<p>"+(qtdCarneTotal * 0.6)+"Kg de Lombo de porco</p><p>"+(qtdCarneTotal * 0.2)+"Kg de linguicinha toscana</p><p>Vinagre de vinho tinto</p><p>Cebola, alho e sal a gosto</p>"
+    let sugsArray = [primeiraSugs, segundaSugs, terceiraSugs, quartaSugs, quintaSugs, sextaSugs, setimaSugs, oitavaSugs, nonaSugs, decimaSugs]
+    
+    return sugsArray[randomSugs]
+    
 }
